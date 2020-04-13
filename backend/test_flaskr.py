ae1767db224762546be1850140f410b0896be950
Noah Ross <noah.baine@gmail.com>
2020-04-13 11:23:17 -0700

    enhanced tests to make sure results are found

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -25,6 +25,12 @@ class TriviaTestCase(unittest.TestCase):
             "category":"1"
         }
 
+        self.category_id_for_testing = 1
+
+        self.search_term = {
+            "searchTerm":"title"
+        }
+
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
@@ -44,16 +50,49 @@ class TriviaTestCase(unittest.TestCase):
     def test_get_categories(self):
         res = self.client().get("/api/categories") #test getting categories
         self.assertEqual(res.status_code,200)
+        response = json.loads(res.data)
+        self.assertTrue(response['success'])
+        self.assertGreater(len(response['categories']), 0)
+        pass
+    
+    def test_get_questions_from_category(self): #test getting questions in a specific category
+        res = self.client().get("/api/categories/{}/questions".format(self.category_id_for_testing))
+        self.assertEqual(res.status_code,200)
+        response = json.loads(res.data)
+        self.assertTrue(response['success'])
+        self.assertGreater(len(response['questions']), 0)
         pass
 
     def test_get_questions(self):
         res = self.client().get("/api/questions") #test getting questions
         self.assertEqual(res.status_code,200)
+        response = json.loads(res.data)
+        self.assertTrue(response['success'])
+        self.assertGreater(len(response['questions']), 0)
         pass
 
     def test_add_question(self):
         res = self.client().post("/api/questions", json=self.add_new_question_data) #test adding a question
         self.assertEqual(res.status_code,200)
+        response = json.loads(res.data)
+        self.assertTrue(response["success"])
+        pass
+
+    def test_delete_question(self):
+        get_last_question = Question.query.order_by(Question.id.desc()).limit(1).all()
+        question_id = get_last_question[0].id
+        res = self.client().delete("/api/questions/{}".format(question_id)) #test deleting a question
+        self.assertEqual(res.status_code,200)
+        response = json.loads(res.data)
+        self.assertTrue(response["success"])
+        pass
+
+    def test_search_questions(self):
+        res = self.client().post("/api/search-questions", json=self.search_term) #test searching
+        self.assertEqual(res.status_code,200)
+        response = json.loads(res.data)
+        self.assertTrue(response["success"])
+        self.assertGreater(len(response['questions']), 0)
         pass
 
     def test_400(self):
@@ -65,7 +104,6 @@ class TriviaTestCase(unittest.TestCase):
         res = self.client().get("/api/cateogriesspelledwrong") #test invalid endpoint
         self.assertEqual(res.status_code,404)
         pass
-
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
